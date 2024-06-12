99dfb7388da489ca2ad9323d8bb8e0cf91214ee8
Rafal Horanin <rafalhoranin@icloud.com>
2024-06-12 15:39:09 +0200

    finalne api

--- old.js
+++ old.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const beerRoutes = require("./routes/beer.js");
 const achievementRoutes = require("./routes/achievements.js");
 const collectedBeerRoutes = require("./routes/collectedBeers.js");
@@ -13,21 +14,24 @@ const swaggerDocs = require('./docs/swaggerConfig');
 const app = express();
 
 // middleware
+app.use(cors()); // Dodaj to, aby zezwolić na CORS
 app.use(express.json());
 
 // define all routes
-app.use('/api', beerRoutes);
-app.use('/api', achievementRoutes);
-app.use('/api', collectedBeerRoutes);
-app.use('/api', userRoutes);
-app.use('/api', storeRoutes);
-app.use('/api', userAchievementRoutes);
-app.use('/api', discussionRoutes);
-app.use('/api', visitedRoutes);
+app.use('/api/beers', beerRoutes);
+app.use('/api/achievements', achievementRoutes);
+app.use('/api/collectedBeers', collectedBeerRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/stores', storeRoutes);
+app.use('/api/userAchievements', userAchievementRoutes);
+app.use('/api/discussions', discussionRoutes);
+app.use('/api/visited', visitedRoutes);
 
 app.use('/api-docs', swaggerDocs.serve, swaggerDocs.setup);
 
-const port = 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
